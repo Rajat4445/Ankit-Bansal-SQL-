fc86d5f31899478030278703a04595b7713e2c45
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2024-06-08 21:36:10 +0530

    Create 1. Derive Points table for ICC tournaments.sql

---
+++ 1. Derive Points table for ICC tournaments.sql
@@ -0,0 +1,15 @@
+-- create a table
+CREATE TABLE matches (
+  team_1 TEXT NOT NULL,
+  team_2 TEXT NOT NULL,
+  winner TEXT NOT NULL
+);
+-- insert some values
+INSERT INTO matches VALUES ('India', 'SL', 'India');
+INSERT INTO matches VALUES ('SL', 'Aus', 'Aus');
+INSERT INTO matches VALUES ('SA', 'Eng', 'Eng');
+INSERT INTO matches VALUES ('Eng', 'NZ', 'NZ');
+INSERT INTO matches VALUES ('Aus', 'India', 'India');
+-- fetch some values
+
+-- Look at the output
